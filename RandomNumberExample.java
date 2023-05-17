59ad431ec5801feec52ab74f34bed967b8345f57
Kumar Ramamoorthy, Senthil <senthil.kumar-ramamoorthy@capgemini.com>
2023-05-18 00:49:00 +0530

    git2Jenkins

--- RandomNumberExample.java
+++ RandomNumberExample.java
@@ -8,6 +8,7 @@ public static void main(String args[])
 System.out.println("1st Random Number: " + Math.random());   
 System.out.println("2nd Random Number: " + Math.random());  
 System.out.println("3rd Random Number: " + Math.random());   
-System.out.println("4th Random Number: " + Math.random());   
+System.out.println("4th Random Number: " + Math.random());
+System.out.println("5th Random Number: " + Math.random());  
 }   
 }  
